084808d6be46e879ba4614e2345643283a6f7f14
Daniel Hicks <danielehicks89@gmail.com>
2025-02-19 16:24:14 -0500

    second commit

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,11 +1,36 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error Loading Albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photoes in the album.
+        </ExpandablePanel>
+      );
+    });
+  }
+
   // console.log(data, error, isLoading);
 
-  return <div>Albums for {user.name}</div>;
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
